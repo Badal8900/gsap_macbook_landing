a05af58c9f9c204bb89c79c4d8bdb8fddc57a7ac
Kpgroup890 <badal.patel@kpigreenenergy.com>
2026-06-29 19:53:16 +0530

    productviewer added, mackbook veriosn added gsap integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
+import ProductViewer from "./components/ProductViewer"
+import {gsap} from "gsap"
+import {ScrollSmoother } from "gsap/all"
+gsap.registerPlugin(ScrollSmoother)
 const App = () => {
 return(
 <main>
 <NavBar />
 <Hero />
+<ProductViewer />
 </main>
 )
 }
